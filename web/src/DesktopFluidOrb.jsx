352d631469f5e4144c20ee58e1a903cd4160ef26
krataratha <kratarathtatran@icloud.com>
2026-08-02 10:28:42 +0530

    Refactor DesktopFluidOrb component for style handling

--- web/src/DesktopFluidOrb.jsx
+++ web/src/DesktopFluidOrb.jsx
@@ -1,26 +1,30 @@
 export default function DesktopFluidOrb({ style = 'fluid' }) {
-  if (style === 'goo') {
+  const currentStyle = style || 'fluid';
+
+  if (currentStyle === 'goo') {
     return (
       <div className="stage goo-orb">
         <div className="stage-scale goo-scale">
           <div className="goo">
-            <div className="fill"></div>
-            <div className="shade"></div>
+            <div className="fill" />
+            <div className="shade" />
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <div className="stage fluid-orb">
       <div className="stage-scale">
         <div className="fluid">
-          <span className="blob b1"></span><span className="blob b2"></span>
-          <span className="blob b3"></span><span className="blob b4"></span>
-          <div className="glass"></div>
+          <span className="blob b1" />
+          <span className="blob b2" />
+          <span className="blob b3" />
+          <span className="blob b4" />
+          <div className="glass" />
         </div>
       </div>
     </div>
-  )
+  );
 }
